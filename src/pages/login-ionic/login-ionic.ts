de5af1814ba669298c490ea4f58e9314856c628d
Anusha <bheemanaanusha8985@gmail.com>
2018-03-29 10:45:43 +0530

    Million dollar commit

---
+++ src/pages/login-ionic/login-ionic.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-login-ionic',
+  templateUrl: 'login-ionic.html'
+})
+export class LoginIonicPage {
+  constructor() {
+
+  }
+}
+
